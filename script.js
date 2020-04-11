f2ea196f57058e94e4ed676d2d3838ecdc107155
nobobobo <ianhsu1221@gmail.com>
2020-04-10 18:49:15 -0700

    added a logic to check at least 1 char is used for each type of characters the user chose + added comments on JS

--- script.js
+++ script.js
@@ -10,51 +10,125 @@ function writePassword() {
 
 }
 
-function generatePassword(){
-  let passLength = parseInt(prompt("Choose a length of the new password: "));
+function generatePassword() {
 
-  while (passLength < 8 || passLength > 128){
-    alert("The length must be between 8 and 128 charcters.")
-    passLength = parseInt(prompt("Choose a length of the new password, again: "));
-  }
-  
-  let hasLower = confirm("Do you want the password to contain lowercase characters?");
-  let hasUpper = confirm("Do you want the password to contain uppercase characters?");
-  let hasNumeric = confirm("Do you want the password to contain numbers?");
-  let hasSpecial = confirm("Do you want the password to contain special characters?");
+  /* 
+  get a length of a new password from user's input. 
+  check and retry if the input is not a number or the length is not between 8 and 128
+  */
 
-  while (!hasLower && !hasUpper && !hasNumeric && !hasSpecial){
-    alert("Please select at least one type of the characters to be contained in the new password!")
-    hasLower = confirm("Select again: Do you want the password to contain lowercase characters?");
-    hasUpper = confirm("Select again: Do you want the password to contain uppercase characters?");
-    hasNumeric = confirm("Select again: Do you want the password to contain numbers?");
-    hasSpecial = confirm("Select again: Do you want the password to contain special characters?");
-  }
+  let passLength = 0;
+
+  do {
+    let res = parseInt(prompt("Choose a length of the new password between 8 and 128: "));
+    
+    if (isNaN(res)){
+      passLength = 0;
+    } else {
+      passLength = res;
+    }
+  } while (passLength < 8 || passLength > 128);
+
+  /* 
+  create booleans to trackt user's choice of types of characters using in a new password.
+  hasLower - new password has lowercase characters
+  hasUpper - new password has uppercase characters
+  hasNumeric - new password has numeric characters
+  hasSpecial - new password has special characters
+  */ 
+
+  let hasLower = false;
+  let hasUpper = false;
+  let hasNumeric = false;
+  let hasSpecial = false;
+
+  do {
+    alert("Please select at least one type of characters to be used in a new password. ")
+    hasLower = confirm("Do you want the password to contain lowercase characters?");
+    hasUpper = confirm("Do you want the password to contain uppercase characters?");
+    hasNumeric = confirm("Do you want the password to contain numbers?");
+    hasSpecial = confirm("Do you want the password to contain special characters?");
+  } while (!hasLower && !hasUpper && !hasNumeric && !hasSpecial);
+
+  /* 
+  create a character set based on choices of type of characters. 
+  for each type of characters, concatenate a list of characters as a string:
+  lowercase - "abcdefghijklmnopqrstuvwxyz"
+  uppercase - "ABCDEFGHIJKLMNOPQRSTUVXWYZ"
+  number - "0123456789"
+  special characters - " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  */
+
 
   let charSet = "";
+  let lower = "abcdefghijklmnopqrstuvwxyz"
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVXWYZ"
+  let numeric = "0123456789";
+  let special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-  if (hasLower){
-    charSet += "abcdefghijklmnopqrstuvwxyz";
+  if (hasLower) {
+    charSet += lower;
   }
 
-  if (hasUpper){ 
-    charSet += "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+  if (hasUpper) {
+    charSet += upper;
   }
 
   if (hasNumeric) {
-    charSet += "0123456789";
+    charSet += numeric;
   }
 
   if (hasSpecial) {
-    charSet += " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    charSet += special;
   }
 
+  /*
+  generate a new password given a character set to be used. 
+  having an object "typeChecker" to track each type of characters is used or not. 
+  */
+ 
   let newPass = "";
-
-  for (let i=0; i<passLength; i++){
-    let charIndex = Math.floor(Math.random()*charSet.length);
-    newPass += charSet[charIndex];
+  let typeChecker = {
+    isLower: false,
+    isUpper: false,
+    isNumeric: false,
+    isSpecial: false
   }
+
+  do {
+    newPass = "";
+    typeChecker.isLower = false;
+    typeChecker.isUpper = false;
+    typeChecker.isNumeric = false;
+    typeChecker.isSpecial = false;
+
+    for (let i = 0; i < passLength; i++) {
+       //Using random index to access the character set.
+      let newChar = charSet[Math.floor(Math.random() * charSet.length)];
+
+      newPass += newChar;
+
+      if (lower.includes(newChar)) {
+        typeChecker.isLower = true;
+      }
+      if (upper.includes(newChar)) {
+        typeChecker.isUpper = true;
+      }
+      if (numeric.includes(newChar)) {
+        typeChecker.isNumeric = true;
+      }
+      if (special.includes(newChar)) {
+        typeChecker.isSpecial = true;
+      }
+    }
+
+  } while (
+    (hasLower && !typeChecker.isLower) ||
+    (hasUpper && !typeChecker.isUpper) ||
+    (hasNumeric && !typeChecker.isNumeric) ||
+    (hasSpecial && !typeChecker.isSpecial)
+  );
+
   return newPass;
 }
 
